139a46d55567c548f23c99dc0e6c000228ed9a54
sally-bugg <sally@gubba.co.nz>
2026-03-08 12:45:10 +1300

    Update auth.login.jsx

--- app/routes/auth.login.jsx
+++ app/routes/auth.login.jsx
@@ -1,3 +1,19 @@
-import { login } from "../shopify.server";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { AppProvider } from "@shopify/shopify-app-remix/react";
+import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
 
-export const loader = ({ request }) => login(request);
+export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
+
+export async function loader() {
+  return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
+}
+
+export default function AuthLogin() {
+  const { apiKey } = useLoaderData();
+  return (
+    <AppProvider isEmbeddedApp apiKey={apiKey}>
+      <div />
+    </AppProvider>
+  );
+}
