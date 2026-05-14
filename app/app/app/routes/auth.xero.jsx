0adbe252ba66cf707f0b719609c2dfc0369a4d70
sally-bugg <sally@gubba.co.nz>
2026-05-14 19:30:25 +1200

    Delete app/app/app/routes directory

--- app/app/app/routes/auth.xero.jsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "@remix-run/node";
-import { authenticate } from "../shopify.server";
-import { getXeroAuthUrl } from "../xero.server";
-
-export async function loader({ request }) {
-  const { session } = await authenticate.admin(request);
-  const url = getXeroAuthUrl(session.shop);
-  return redirect(url);
-}
