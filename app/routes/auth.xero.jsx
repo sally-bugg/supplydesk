358bdf1a3e06c5a3519cb806c0254b16f363f529
sally-bugg <sally@gubba.co.nz>
2026-05-14 20:46:57 +1200

    Update auth.xero.jsx

--- app/routes/auth.xero.jsx
+++ app/routes/auth.xero.jsx
@@ -1,9 +1,9 @@
 import { redirect } from "@remix-run/node";
-import { authenticate } from "../shopify.server";
 import { getXeroAuthUrl } from "../xero.server";
 
 export async function loader({ request }) {
-  const { session } = await authenticate.admin(request);
-  const url = getXeroAuthUrl(session.shop);
-  return redirect(url);
+  const url = new URL(request.url);
+  const shop = url.searchParams.get("shop");
+  if (!shop) return redirect("/app/settings?xero=error");
+  return redirect(getXeroAuthUrl(shop));
 }
